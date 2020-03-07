ebabf91c5b93126c136a7bf357fb835339354a98
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-07 15:09:55 -0800

    views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello World. This is the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return HttpResponse(', '.join([q.question_text for q in latest_question_list]))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results for question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
